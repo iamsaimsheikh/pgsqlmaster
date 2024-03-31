ec5c0fe5e53b7bf87b3319774428ed299a661b03
Mehreen <iamsaimsheikh@gmail.com>
2024-03-31 19:09:31 +0500

    add seed and finish project

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+const user = require('../db/models/user');
+const asyncHandler = require('express-async-handler');
+const { successMessage } = require('../utils/helper');
+
+const getAllUser = asyncHandler(async (req, res, next) => {
+    const users = await user.findAndCountAll({
+        where: {
+            userType: {
+                [Sequelize.Op.ne]: '0',
+            },
+        },
+        attributes: { exclude: ['password'] },
+    });
+    return res.status(200).json(successMessage("Fetched all users", users));
+});
+
+module.exports = { getAllUser };
